cf6214efd517dad393f2cfc7440813933d57e056
Luca Favatella <luca.favatella@erlang-solutions.com>
2018-12-10 22:58:47 +0000

    Prevent .git dir of prebuilt miners repo from being included in OTP release

--- src/rebar_aecuckooprebuilt_app_with_priv_from_git_resource.erl
+++ src/rebar_aecuckooprebuilt_app_with_priv_from_git_resource.erl
@@ -9,11 +9,31 @@
 
 -define(RESOURCE_TYPE, aecuckooprebuilt_app_with_priv_from_git).
 -define(APP_NAME, <<"aecuckooprebuilt">>).
+
 -define(APP_DESC, "").
 -define(APP_VSN, "0.1.0").
 
+-define(MAKEFILE, <<
+".PHONY: forced-priv
+forced-priv: rm-priv
+	cp -pR priv_src priv
+	rm -rf priv/.git
+
+.PHONY: rm-priv
+rm-priv:
+	rm -rf priv
+
+.PHONY: clean
+clean: rm-priv ;
+">>).
+
+-define(REBAR_CONFIG, <<
+"{pre_hooks, [{compile, \"make -s forced-priv\"}]}.
+{post_hooks, [{clean, \"make clean\"}]}.
+">>).
+
 lock(AppDir, {?RESOURCE_TYPE, GitSource}) ->
-    {?RESOURCE_TYPE, rebar_git_resource:lock(priv_dir(AppDir), GitSource)}.
+    {?RESOURCE_TYPE, rebar_git_resource:lock(priv_src_dir(AppDir), GitSource)}.
 
 download(Dir, Source, State) ->
     %% Custom dep resources in rebar3 pre-3.7.0 do not have access to
@@ -21,27 +41,43 @@ download(Dir, Source, State) ->
     download_(Dir, ?APP_NAME, Source, State).
 
 download_(Dir, AppName, {?RESOURCE_TYPE, GitSource}, State) when is_binary(AppName) ->
-    AppSrcBin = minimal_app_src(AppName, ?APP_DESC, ?APP_VSN),
-    AppSrcFile = app_src_file(Dir, AppName),
-    ok = filelib:ensure_dir(AppSrcFile),
-    %% Force write
-    case file:write_file(AppSrcFile, AppSrcBin) of
+    Fs = [{filename:join(Dir, "Makefile"), ?MAKEFILE},
+          {filename:join(Dir, "rebar.config"), ?REBAR_CONFIG},
+          {app_src_file(Dir, AppName), minimal_app_src(AppName, ?APP_DESC, ?APP_VSN)}
+         ],
+    case force_write_files(Fs) of
         ok ->
-            rebar_git_resource:download(priv_dir(Dir), GitSource, State);
+            rebar_git_resource:download(priv_src_dir(Dir), GitSource, State);
         {error, _} = Err ->
             Err
     end.
 
 needs_update(Dir, {?RESOURCE_TYPE, GitSource}) ->
-    rebar_git_resource:needs_update(priv_dir(Dir), GitSource).
+    rebar_git_resource:needs_update(priv_src_dir(Dir), GitSource).
 
 make_vsn(Dir) ->
-    rebar_git_resource:make_vsn(priv_dir(Dir)).
+    rebar_git_resource:make_vsn(priv_src_dir(Dir)).
 
 %%% Internal functions
 
-priv_dir(Dir) ->
-    filename:join(Dir, "priv").
+%% From http://erlang.org/doc/design_principles/applications.html#directory-structure
+%% > Directories with `_src` suffix indicates that it is a part of the application and the compilation step.
+priv_src_dir(Dir) ->
+    filename:join(Dir, "priv_src").
+
+force_write_files(Files) ->
+    force_write_files_(Files, ok).
+
+force_write_files_(_, {error, _} = Err) ->
+    Err;
+force_write_files_([], ok) ->
+    ok;
+force_write_files_([{Filename, Bytes} | T], ok) ->
+    force_write_files_(T, force_write_file(Filename, Bytes)).
+
+force_write_file(Filename, Bytes) ->
+    ok = filelib:ensure_dir(Filename),
+    file:write_file(Filename, Bytes).
 
 minimal_app_src(AppName, Desc, Vsn) when is_binary(AppName),
                                          is_list(Desc), is_list(Vsn) ->
@@ -51,11 +87,11 @@ minimal_app_src(AppName, Desc, Vsn) when is_binary(AppName),
     %% rebar3 fills in the modules key.
     Template = <<
 "{application, {{name}},
-  [{description, \"{{desc}}\"},
-   {vsn, \"{{vsn}}\"},
-   {registered, []},
-   {applications, []}
-  ]}.
+ [{description, \"{{desc}}\"},
+  {vsn, \"{{vsn}}\"},
+  {registered, []},
+  {applications, []}
+ ]}.
 ">>,
     Context = [{name, AppName}, {desc, Desc}, {vsn, Vsn}],
     rebar_templater:render(Template, Context).
@@ -65,4 +101,3 @@ app_src_file(Dir, AppName) ->
 
 src_dir(Dir) ->
     filename:join(Dir, "src").
-
